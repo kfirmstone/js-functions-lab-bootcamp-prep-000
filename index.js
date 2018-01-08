589dc3c6afd15bfd05cffc313252e2cf775a7450
Kyle Firmstone <kfirmstonecpt@gmail.com>
2018-01-08 03:56:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 // write your code below!
-function happyHolidays()
-{
+function happyHolidays() {
   return "Happy holidays!"
 }
-function happyHolidaysTo(name)
-{
+function happyHolidaysTo(name) {
   return ("Happy holidays, ${name}!")
 }
-function happyHolidaysTo(holiday,name)
-{
+function happyHolidaysTo(holiday,name) {
   return ("Happy ${holiday}, ${name}!")
 }
-function holidayCountdown(holiday, days)
-{
+function holidayCountdown(holiday, days) {
   return "It's ${days} days until ${holiday}!"
-}
 }
