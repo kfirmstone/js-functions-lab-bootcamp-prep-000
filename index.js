2d6b0684568da0a0d9df2b81a10f63296253aaeb
Kyle Firmstone <kfirmstonecpt@gmail.com>
2018-01-08 03:45:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // write your code below!
 function happyHolidays()
+{
+  return "Happy holidays!"
+}
 function happyHolidaysTo("Ashley")
 function happyHolidaysTo("Christmas","Ashley")
 function holidayCountdown("Christmas", 25)
