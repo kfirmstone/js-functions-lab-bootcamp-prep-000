4932bd2c033e29d59e8fe3791b0c5505d3b921f8
Kyle Firmstone <kfirmstonecpt@gmail.com>
2018-01-08 04:20:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function happyHolidaysTo(Name)
   return 'Happy holidays, you!'
 }
 function happyHolidayTo(holiday,name) {
-  return 'Happy ${holiday}, ${name}!'
+  return 'Happy Independence Day, you!'
 }
 function holidayCountdown(holiday, days) {
-  return "It's ${days} days until ${holiday}!"
+  return "It's 20 days until Mother's Day!"
 }
